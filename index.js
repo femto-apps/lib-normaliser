e25073138466798182739b72f875b0ec1152b96e
Alexander Craggs <popey@debenclipper.com>
2019-04-22 02:16:14 +0100

    Update normaliser to go over each item in template instead of object

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ const _ = require('lodash')
 function normalise(template, object) {
     const results = {}
 
-    for (let index of Object.keys(object)) {
-        if (index in template) {
-            const [error, result] = template[index](object[index])
+    for (let index of Object.keys(template)) {
+        const [error, result] = template[index](object[index])
 
-            if (error) {
-                _.set(results, `error.${index}`, error)
-            } else {
-                results[index] = result
-            }
+        if (error) {
+            _.set(results, `error.${index}`, error)
+        } else {
+            results[index] = result
         }
     }
 
