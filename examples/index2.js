49ed995481c8fe4975ecb36bcbc66c3ee973d71a
aappleton8 <aappleton8@users.noreply.github.com>
2020-10-05 19:46:20 +0100

    Update the new examples.
    Signed-off-by: aappleton8 <aappleton8@users.noreply.github.com>

--- examples/index2.js
+++ examples/index2.js
@@ -20,34 +20,35 @@ const normaliser = {
 {
     const { type, value, error } = normalise(normaliser, { type: 'string1', value: 'string2' })
     console.log(type, value, error)
-    // error: { name: ['Name too short'] } 
-    // name: undefined
 }
 
 {
     const { type, value, error } = normalise(normaliser, { type: 3.45, value: 3.45 })
     console.log(type, value, error)
-    // error: undefined
-    // name: A_very_good_user
 }
 
 {
     const { type, value, error } = normalise(normaliser, { type: 3.45, value: 33.45 })
     console.log(type, value, error)
-    // error: undefined
-    // name: A_very_good_user
 }
 
 {
     const { type, value, error } = normalise(normaliser, { type: 5, value: 5 })
     console.log(type, value, error)
-    // error: undefined
-    // name: A_very_good_user
 }
 
 {
     const { type, value, error } = normalise(normaliser, { type: 22, value: 22 })
     console.log(type, value, error)
-    // error: undefined
-    // name: A_very_good_user
 }
+
+/*
+Outputs: 
+
+undefined 'string2' { type: [ 'Data is not an integer' ] }
+undefined undefined { type: [ 'Data is not an integer' ], value: [ 'Data must be at least 10' ] }
+undefined 33.45 { type: [ 'Data is not an integer' ] }
+5 undefined { value: [ 'Data must be at least 10' ] }
+22 22 undefined
+*/
+
